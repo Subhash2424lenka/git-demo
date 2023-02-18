a3cbd0a339b1b114123ca8cdbeac4777f4a2a89a
UserTwo <crzogrant@gmail.com>
2023-02-18 13:46:37 +0530

    new

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <h1>Version One</h1>
 <h1>Version Two</h1>
 <h3>Version three</h3>
 
+=======
+<h1>version two</h1>
+>>>>>>> parent of 996a2b2 (new addon)
 =======
 <h1>version two</h1>
 >>>>>>> parent of 996a2b2 (new addon)
